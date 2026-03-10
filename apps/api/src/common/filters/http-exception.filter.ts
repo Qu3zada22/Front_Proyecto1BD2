ba224d02e9d03842a6186a14caa00319dabeaa8c
Jonialen <jonathanalej09@gmail.com>
2026-03-10 13:42:58 -0600

    debug: log exceptions in filter to diagnose upload 500

--- apps/api/src/common/filters/http-exception.filter.ts
+++ apps/api/src/common/filters/http-exception.filter.ts
@@ -14,6 +14,8 @@ export class AllExceptionsFilter implements ExceptionFilter {
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
 
+    console.error('[AllExceptionsFilter]', exception);
+
     let status = HttpStatus.INTERNAL_SERVER_ERROR;
     let message: string | string[] = 'Error interno del servidor';
 
